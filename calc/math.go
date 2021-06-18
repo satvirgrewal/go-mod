e489b9971d40f9869445e1cc08c4db1be609ff63
Satvir Grewal <satvir.grewal03@gmail.com>
2021-06-17 23:05:44 -0700

    new release version v2.0.0

--- calc/math.go
+++ calc/math.go
@@ -1,9 +1,15 @@
 package calc
 
-func Add(nums ...int) int {
+import "errors"
+
+func Add(nums ...int) (error, int) {
 	sum := 0
+	if len(nums) < 2 {
+		err := errors.New("")
+		return err, sum
+	}
 	for _, val := range nums {
 		sum += val
 	}
-	return sum
+	return nil, sum
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/satvirgrewal/go-mod
+module github.com/satvirgrewal/go-mod/v2
 
 go 1.16
